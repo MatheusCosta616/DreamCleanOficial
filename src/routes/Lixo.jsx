014c1d84847420b25a642306819bf2ba87764452
Rodrigo Lima <rodrigo.blima17@gmail.com>
2023-09-17 12:07:58 -0300

    v7

--- src/routes/Lixo.jsx
+++ src/routes/Lixo.jsx
@@ -10,7 +10,7 @@ export default function Lixo() {
 
                 <h1 className="dDiv">Como o lixo é descartado:</h1>
 
-                <p className="txtPadr">Através de nossos bueiros inteligentes, dos quais possuem o mecanismo de filtração de água,
+                <p className="txtLixo">Através de nossos bueiros inteligentes, dos quais possuem o mecanismo de filtração de água,
                     conseguimos monitorar as regiões que possuem mais lixo acumulado, com isso nós temos acesso a estas áreas
                     podendo coletar o lixo com nossos caminhões de coleta, fazendo com que as ruas mantenham seu fluxo de escoamento
                     de água, impedindo que futuras enchentes ocorram nas cidades.
